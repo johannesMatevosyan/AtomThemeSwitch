7610923f294f023ffc1b9fbd6b21288dd93dded7
johannesMatevosyan <h.matevosyan@yahoo.com>
2024-06-13 13:07:16 +0400

    updated description for storybook framework

--- src/lib/AtomThemeSwitch/AtomThemeSwitch.stories.tsx
+++ src/lib/AtomThemeSwitch/AtomThemeSwitch.stories.tsx
@@ -1,22 +1,81 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 import { AtomThemeSwitch } from './AtomThemeSwitch';
 
 const meta: Meta<typeof AtomThemeSwitch> = {
-    component: AtomThemeSwitch
+    title: 'Switch theme',
+    component: AtomThemeSwitch,
+    argTypes: {
+        mode: {
+            type: 'string',
+            description: 'Different sizes available',
+            defaultValue: 'material',
+            options: ['material', 'space'],
+        },
+        size: {
+            type: 'string',
+            description: 'Different sizes available',
+            defaultValue: 'medium',
+            options: ['small', 'medium', 'large'],
+            control: {
+                type: 'radio'
+            }
+        },
+        type: {
+            type: 'string',
+            description: 'Type of switch',
+            defaultValue: 'round',
+            options: ['round', 'square'],
+            control: {
+                type: 'radio'
+            }
+        }
+    }
 }
 
 export default meta;
 
-type Story = StoryObj<typeof AtomThemeSwitch>
-
-export const Default: Story = {
-    args: {
-        children: 'Switch Theme to dark'
-    }
+interface ButtonProps {
+    type: string,
+    size: string,
+    mode: string,
 }
 
-export const Dark: Story = {
-    args: {
-        children: 'Dark theme set'
-    }
+const Template: StoryFn<ButtonProps> = (args: any) => <AtomThemeSwitch {...args} />
+
+export const Default = Template.bind({});
+export const Small = Template.bind({});
+export const Large = Template.bind({});
+
+Default.args = {
+    type: 'round',
+    size: 'medium',
+    mode: 'material',
 }
+
+Small.args = {
+    type: 'round',
+    size: 'small',
+    mode: 'material',
+}
+
+
+Large.args = {
+    type: 'round',
+    size: 'large',
+    mode: 'material',
+}
+
+
+// type Story = StoryObj<typeof AtomThemeSwitch>
+
+// export const Default: Story = {
+//     args: {
+//         children: 'Switch Theme to dark'
+//     }
+// }
+
+// export const Dark: Story = {
+//     args: {
+//         children: 'Dark theme set'
+//     }
+// }
