6a32481303fed594298500175a05ffce956b84e4
johannesMatevosyan <h.matevosyan@yahoo.com>
2024-06-18 18:08:28 +0400

    modified features to update CSS root properties dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { AtomThemeSwitch, AtomThemeSwitchContext } from 'atom-theme-switch'
 import { ThemeType } from './lib/AtomThemeSwitch/models';
+import { useEffect } from 'react';
 
 function App() {
 
+  useEffect(() => {
+    document.documentElement.style.setProperty('--switch-height', '46px');
+  }, [])
   const fixedPosition = {position: 'fixed', top: '0', left: '0', bottom: '0', right: '0', margin: 'auto'}
   const customMatTheme = {
     checked: {
