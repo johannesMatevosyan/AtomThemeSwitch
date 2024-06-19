878418f6b2ef0a6eaa59b0ee02e33cf34a82c881
johannesMatevosyan <h.matevosyan@yahoo.com>
2024-06-19 14:09:56 +0400

    added 'IThemeCustomColors' and 'IFixedPosition' interfaces

--- src/lib/AtomThemeSwitch/models.ts
+++ src/lib/AtomThemeSwitch/models.ts
@@ -9,27 +9,28 @@ export interface AWSContextType {
     setTheme: (name: ThemeType) => void;
 }
 
+export interface IThemeCustomColors {
+    bgColor?: string, 
+    color?: string
+}
+
+export interface IFixedPosition {
+    position?: string,
+    top?: string,
+    right?: string,
+    left?: string,
+    bottom?: string,
+    margin?: string,
+    transform?: string,
+}
+
 export interface IAtomThemeSwitch {
-    size: string;
     mode: string;
     type: string;
-    fixedPosition?: {
-        position?: string,
-        top?: string,
-        right?: string,
-        left?: string,
-        bottom?: string,
-        margin?: string,
-    };
+    fixedPosition?: IFixedPosition;
     customMatTheme?: {
-        checked?: {
-            bgColor?: string, 
-            color?: string
-          },
-        unchecked?: {
-            bgColor?: string, 
-            color?: string,
-        }
+        checked?: IThemeCustomColors,
+        unchecked?: IThemeCustomColors
     }
     checked?: boolean,
     onChanged?: (type: ThemeType) => void;
