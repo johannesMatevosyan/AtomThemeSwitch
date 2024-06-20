a10b5221481d49c1d5ea9a659fc130730edb96e3
johannesMatevosyan <h.matevosyan@yahoo.com>
2024-06-20 16:53:33 +0400

    moved 'ATSContextProps' type to separate file

--- src/lib/AtomThemeSwitch/store/AtomThemeSwitchContext.tsx
+++ src/lib/AtomThemeSwitch/store/AtomThemeSwitchContext.tsx
@@ -1,20 +1,16 @@
-import { ReactElement, ReactNode, useEffect, useState } from "react";
-import { AWSContextType, ThemeType } from "../models";
+import { ReactElement, useEffect, useState } from "react";
+import { AWSContextType, ThemeType, ATSContextProps } from "../models";
 import ThemeDataContext from "./ThemeDataContext";
 import { setThemeName, getThemeName } from "../theme-manager";
 
-type ATSProps = {
-  selectedTheme?: string;
-  children: ReactNode;
-};
 
-export function AtomThemeSwitchContext(props: ATSProps): ReactElement<AWSContextType> {
+export function AtomThemeSwitchContext(props: ATSContextProps): ReactElement<AWSContextType> {
     const { children, selectedTheme } = props;
     const [theme, setTheme] = useState<`${ThemeType}`>(ThemeType.LIGHT);
 
     useEffect(() => {
         const result = getThemeName(selectedTheme)
-        if(!result) { // theme name is not defined then set to 'light'
+        if(!result) { // if theme name is not defined then set to 'light'
           setThemeName(selectedTheme, ThemeType.LIGHT)
         } else if (result === ThemeType.LIGHT) {
           setTheme(ThemeType.LIGHT);
